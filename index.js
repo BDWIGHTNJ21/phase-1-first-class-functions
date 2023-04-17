a5164e83f7aaa4f60c227068b3413f95b0f8b91d
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-04-16 21:21:26 -0400

    completed

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function receivesAFunction(callback){
+    return callback();
+}
+function Denisefunction() {
+    console.log("hello")
+}
+function returnsANamedFunction() {
+  return Denisefunction
+
+ }
+
+
+function returnsAnAnonymousFunction(){
+    return function(){
+
+    }
+}
+
+
